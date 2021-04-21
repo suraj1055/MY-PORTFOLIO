8c2c94d755f2683ccbd4807dab24b63a9a658537
Suraj vishwakarma <vishwakarmasuraj631@gmail.com>
2021-04-21 13:52:11 +0530

    Updated Navbar

--- js/navigation.js
+++ js/navigation.js
@@ -23,4 +23,17 @@ let isActive = false;
 
 Hamburger.addEventListener('click', function(){
     MobileNav.classList.toggle('open');
+
+    if(!isActive){
+        bars[0].style.transform = 'rotate(45deg)';
+        bars[1].style.opacity ='0';
+        bars[2].style.transform = 'rotate(-45deg)';
+        isActive = true;
+    }
+    else{
+        bars[0].style.transform = 'rotate(0deg)';
+        bars[1].style.opacity = '1';
+        bars[2].style.transform = 'rotate(0deg)';
+        isActive = false;
+    }
 })
